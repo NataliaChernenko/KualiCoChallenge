abec95e6b0ed1cec3f6182d1c5b48b75d0a57833
NataliaChernenko <greenkraken@gmail.com>
2015-12-23 16:56:55 -0500

    Update Controller.java
    Added a constructor

--- Controller.java
+++ Controller.java
@@ -7,5 +7,23 @@
 */
 
 public class Controller {
+  
+  private Elevator[] elevators;
+  
+  /* Creates a Controller for the specified number of elevators
+  */
+  public Controller(int numElevators, int maxFloors){
+    if (numElevators > 0 && maxFloors > 0) {
+      elevators = new Elevator[numElevators]
+      for(int i = 0; i < numElevators; i++){
+        elevators[i] = new Elevator(maxFloors);
+      }
+    }
+    else {
+      throw IllegalArgumentException("invalid arguments to Controller constructor")
+    }
+  }
+  
+  
 
 }
